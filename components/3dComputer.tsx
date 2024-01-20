4ba6293e46255781b010d421ea97e59a91a35ecc
altf4rex <mr.stereo2002@gmail.com>
2024-01-20 21:44:11 +0300

    3d

--- components/3dComputer.tsx
+++ components/3dComputer.tsx
@@ -1,43 +1,26 @@
 "use client"
 import { Canvas } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
-import { motion } from 'framer-motion-3d'
+import { motion} from 'framer-motion-3d'
+import { PerspectiveCamera } from '@react-three/drei'
+import { MotionValue } from 'framer-motion';
 
-function Model() {
-    const { scene } = useGLTF('/images/computer.glb')
+
+  export default function Computer({mouseX, mouseY}: {mouseX: MotionValue<number>, mouseY: MotionValue<number>}) {
+
+    function Model() {
+      const { scene } = useGLTF('/images/computer.glb')
+      return (
+        <motion.primitive 
+          object={scene} 
+          rotation={[mouseY, mouseX, 0]}
+        />
+      )
+    }
+
     return (
-      <motion.primitive 
-        object={scene} 
-        scale={4}
-        animate={["hover"]}
-        variants={{
-          unliked: {
-            x: [0, 0],
-            y: [0, 0],
-            scale: 0.9
-          },
-          liked: {
-            x: 4,
-            y: [0, -1.5, 2],
-            scale: 0.9,
-            transition: { duration: 0.5 }
-          },
-          hover: {
-            rotateZ: 0,
-            rotateY: 0.3,
-            scale: 1.3,
-            transition: {
-              rotateZ: { duration: 1.5, ease: "linear", repeat: Infinity }
-            }
-          }
-        }}
-      />
-    )
-  }
-  
-  export default function Computer() {
-    return (
-      <Canvas className='w-screen h-screen'>
+      <Canvas>
+        <PerspectiveCamera makeDefault position={[0.2, 0.2, 1.2]} />
         <ambientLight />
         <pointLight position={[10, -10, 10]} />
         <Model />
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,17 @@
 "use client"
-import { motion, useScroll, useTransform} from "framer-motion"
-import { useRef } from "react"
+import { motion, useScroll, useTransform, useMotionValue} from "framer-motion"
+import { useRef, useState } from "react"
 import Computer from "./3dComputer";
-
+import useMeasure from "react-use-measure";
 export default function Main() {
+
+    const sensitivity = 0.01;
+
+    const [isDragging, setIsDragging] = useState(false)
+    const [ref3d, bounds] = useMeasure({ scroll: false });
+    const mouseX = useMotionValue(0);
+    const mouseY = useMotionValue(-0.5);
+
     let ref = useRef(null);
     let { scrollYProgress } = useScroll({
         target: ref,
@@ -11,9 +19,24 @@ export default function Main() {
     })
     let y = useTransform(scrollYProgress, [0, 1], ["0%", "80%"])
     return (
-        <motion.section className="flex pl-[2vw] overflow-hidden relative z-0 max-md:pl-0" ref={ref} style={{y}}>
+        <motion.section className="h-screen flex items-center pl-[2vw] overflow-hidden relative z-0 max-md:pl-0" ref={ref} style={{y}}>
             <h1 className="header">DN</h1>
-            <Computer/>
+            <motion.div
+            ref={ref3d}
+            onPointerMove={(e) => {
+                if (isDragging) {
+                    mouseX.set((e.clientX - bounds.x - bounds.width / 2) * sensitivity);
+                    mouseY.set((e.clientY - bounds.y - bounds.height / 2) * sensitivity);
+                }
+              }}
+              onTapStart={() => setIsDragging(true)}
+              onTap={() => setIsDragging(false)}
+            >
+                <Computer 
+                    mouseX={mouseX}
+                    mouseY={mouseY}/>
+            </motion.div>
+
         </motion.section>
     )
 }
