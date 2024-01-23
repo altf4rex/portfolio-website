6631237199e80dbdf89eee37183808361f9818dc
altf4rex <mr.stereo2002@gmail.com>
2024-01-23 18:53:01 +0300

    isMobile

--- components/3dComputer.tsx
+++ components/3dComputer.tsx
@@ -1,14 +1,40 @@
 "use client"
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-import { Suspense } from 'react'
-import { Text } from '@react-three/drei';
+import { Suspense, useEffect, useState } from 'react'
+
   export default function Computer() {
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+      // Add a listener for changes to the screen size
+      const mediaQuery = window.matchMedia("(max-width: 1536px)");
+  
+      // Set the initial value of the `isMobile` state variable
+      setIsMobile(mediaQuery.matches);
+  
+      // Define a callback function to handle changes to the media query
+      const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+        setIsMobile(event.matches);
+      };
+  
+      // Add the callback function as a listener for changes to the media query
+      mediaQuery.addEventListener("change", handleMediaQueryChange);
+  
+      // Remove the listener when the component is unmounted
+      return () => {
+        mediaQuery.removeEventListener("change", handleMediaQueryChange);
+      };
+    }, []);
+  
 
     function Model() {
       const { scene } = useGLTF('/images/computer.glb')
       return (
-      <mesh position={[0.2, -0.2, 0]}>
+      <mesh 
+      scale={isMobile ? 0.7 : 1}
+      position={isMobile ? [0.23, -0.12, 0.1] : [0.15, -0.2, 0]}>
         <primitive 
           object={scene}
         />
@@ -20,30 +46,18 @@ import { Text } from '@react-three/drei';
       <Canvas
       frameloop='demand'
       shadows
-      camera={{position: [0.1, 0.1, 1.2], fov: 50}}
+      camera={{position: [0.8, 0.2, 1.1], fov: 50}}
       >
         <Suspense>
           <OrbitControls 
             enablePan={false}
-            enableZoom={true}
+            enableZoom={false}
             enableRotate={true}
             />
-              <ambientLight intensity={2} />
-              <spotLight position={[0.2, -0.2, 1]} angle={Math.PI / 4} penumbra={1} intensity={0.5} castShadow />
-            <Model /> 
-            <Text
-
-          position={[0.2, 0.2, 0.222]}
-          color="white"
-          fontSize={0.2}
-          maxWidth={1}
-          lineHeight={1}
-          letterSpacing={0.02}
-          textAlign="left"
-        >
-         :(
-        </Text>
-            <Preload all /> 
+              <ambientLight intensity={1} />
+              <spotLight position={[0.2, -0.2, 1]} angle={Math.PI / 4} penumbra={1} intensity={1.5} castShadow />
+          <Model /> 
+          <Preload all /> 
         </Suspense>
       </Canvas>
     )
