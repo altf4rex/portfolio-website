cc149466976ef3950f30f0e3741fc15904320fa8
altf4rex <mr.stereo2002@gmail.com>
2024-01-29 23:27:21 +0300

    fix

--- components/3dComputer.tsx
+++ components/3dComputer.tsx
@@ -1,49 +1,17 @@
 "use client"
 import { Canvas  } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-import { Suspense, useEffect, useState } from 'react'
+import { Suspense } from 'react'
 
   export default function Computer() {
 
-    const [isTablet, setIsTablet] = useState(false);
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-      // Add a listener for changes to the screen size
-      const mediaQueryTablet = window.matchMedia("(max-width: 1366px)");
-      const mediaQueryMobile = window.matchMedia("(max-width: 767px)");
-    
-      // Set state variable
-      setIsTablet(mediaQueryTablet.matches);
-      setIsMobile(mediaQueryMobile.matches);
-    
-      // Define a callback function to handle changes to the media query
-      const handleTabletChange = (event: MediaQueryListEvent) => {
-        setIsTablet(event.matches);
-      };
-    
-      const handleMobileChange = (event: MediaQueryListEvent) => {
-        setIsMobile(event.matches);
-      };
-    
-      // Add the callback function as a listener for changes to the media query
-      mediaQueryTablet.addEventListener("change", handleTabletChange);
-      mediaQueryMobile.addEventListener("change", handleMobileChange);
-    
-      // Remove the listener when the component is unmounted
-      return () => {
-        mediaQueryTablet.removeEventListener("change", handleTabletChange);
-        mediaQueryMobile.removeEventListener("change", handleMobileChange);
-      };
-    }, []);
-    
     function Model() {
-      const { scene } = useGLTF('/images/3dcomputer.glb');
+      const { scene } = useGLTF('/images/untitled.glb');
       
       return (
       <mesh 
-      scale={isMobile ? 0.6 : isTablet ? 0.8 : 1}
-      position={isMobile ? [0.15, -0.5, 0] : [-0.02, -0.46, 0]}>
+      scale={0.2}
+      position={[-0.02, -0.46, 0]}>
         <primitive 
           object={scene}
         />
@@ -55,13 +23,13 @@ import { Suspense, useEffect, useState } from 'react'
       <Canvas
       frameloop='demand'
       shadows
-      camera={{position: [0.64, -0.25, 1.2], fov: 50}}
+      camera={{position: [0.64, -0.1, 1.15], fov: 50}}
       >
         <Suspense>
         <OrbitControls 
         enablePan={false}
         enableZoom={false}
-        enableRotate={isTablet ? false : true}
+        enableRotate={true}
         />
               <ambientLight intensity={1} />
               <spotLight position={[0.2, -0.2, 1]} angle={Math.PI / 4} penumbra={1} intensity={1.5} castShadow />
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,36 +1,98 @@
-"use client"
-import { motion, useScroll, useTransform} from "framer-motion"
-import { useRef } from "react"
+"use client";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 import Computer from "./3dComputer";
 import Header from "./Header";
+import { useEffect, useState } from 'react'
+import Image from "next/image";
 
 export default function Main() {
+  let ref = useRef(null);
+  let { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  let y = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
 
-    let ref = useRef(null);
-    let { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ["start start", "end start"]
-    })
-    let y = useTransform(scrollYProgress, [0, 1], ["0%", "80%"])
-    return (
-        <motion.section className="h-screen flex flex-col overflow-hidden relative z-0" ref={ref} style={{y}}>
-            <div className="hidden max-[1366px]:block max-[1366px]:w-screen max-[1366px]:h-screen max-[1366px]:absolute max-[1366px]:top-0 max-[1366px]:left-0 max-[1366px]:z-10"></div>
-            <Header />
-            <h1 className="header absolute z-0 left-20 top-[62%] -translate-y-1/2 max-lg:left-2 max-lg:top-[36%] max-md:top-[34%] max-md:-left-9">DN</h1> 
-            <div className="hidden absolute bottom-12 left-3 max-h-[36vh] max-md:flex  max-sm:bottom-24 ">
-            <svg width="20" height="367" viewBox="0 0 20 367" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10.9998 342L14.9998 346L18.9998 342" stroke="#B2B2B1"/>
-                    <path d="M10.9998 262L14.9998 266L18.9998 262" stroke="#B2B2B1"/>
-                    <path d="M10.9998 352L14.9998 356L18.9998 352" stroke="#B2B2B1"/>
-                    <path d="M10.9998 302L14.9998 306L18.9998 302" stroke="#B2B2B1"/>
-                    <path d="M10.9998 362L14.9998 366L18.9998 362" stroke="#B2B2B1" strokeOpacity="0.9"/>
-                    <path d="M10.9998 312L14.9998 316L18.9998 312" stroke="#B2B2B1"/>
-                    <path d="M1 367L0.999984 -5.24521e-06" stroke="#B2B2B1" strokeDasharray="4 4"/>
-                </svg>
-                <p className="text-main">Front-end is not just a job, <br/> 
-                it&apos;s a mindset.</p>
-            </div>
-            <Computer />
-        </motion.section>
-    )
+
+  const [isTablet, setIsTablet] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQueryTablet = window.matchMedia("(max-width: 1366px)");
+  
+    // Set state variable
+    setIsTablet(mediaQueryTablet.matches);
+  
+    // Define a callback function to handle changes to the media query
+    const handleTabletChange = (event: MediaQueryListEvent) => {
+      setIsTablet(event.matches);
+    };
+  
+    // Add the callback function as a listener for changes to the media query
+    mediaQueryTablet.addEventListener("change", handleTabletChange);
+  
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQueryTablet.removeEventListener("change", handleTabletChange);
+    };
+  }, []);
+  
+
+  return (
+    <section
+      className="w-screen h-screen top-0 left-0 relative z-0 flex flex-col overflow-hidden">
+        <motion.div
+        className="w-screen h-screen absolute"
+        ref={ref}
+        style={{ y }}
+        >
+      <Header />
+      <h1 className="header absolute z-0 left-20 top-[62%] -translate-y-1/2 max-lg:left-2 max-lg:top-[36%] max-md:top-[34%] max-md:-left-9">
+        DN
+      </h1>
+      <div className="hidden absolute bottom-12 left-3 max-h-[36vh] max-md:flex  max-sm:bottom-24 ">
+        <svg
+          width="20"
+          height="367"
+          viewBox="0 0 20 367"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M10.9998 342L14.9998 346L18.9998 342" stroke="#B2B2B1" />
+          <path d="M10.9998 262L14.9998 266L18.9998 262" stroke="#B2B2B1" />
+          <path d="M10.9998 352L14.9998 356L18.9998 352" stroke="#B2B2B1" />
+          <path d="M10.9998 302L14.9998 306L18.9998 302" stroke="#B2B2B1" />
+          <path
+            d="M10.9998 362L14.9998 366L18.9998 362"
+            stroke="#B2B2B1"
+            strokeOpacity="0.9"
+          />
+          <path d="M10.9998 312L14.9998 316L18.9998 312" stroke="#B2B2B1" />
+          <path
+            d="M1 367L0.999984 -5.24521e-06"
+            stroke="#B2B2B1"
+            strokeDasharray="4 4"
+          />
+        </svg>
+        <p className="text-main">
+          Front-end is not just a job, <br />
+          it&apos;s a mindset.
+        </p>
+      </div>
+      {
+        !isTablet ? 
+        <Computer /> : 
+          <Image
+            className="min-w-[68%] min-h-[48%] absolute max-[1366px]:-bottom-24 max-[1366px]:-right-[30rem] max-lg:-bottom-8 max-lg:-right-80 max-md: max-md:min-w-[120%] max-md:min-h-[50%] max-md:-bottom-8 max-md:-right-40"
+            src="/images/Image_computer4.png"
+            alt="computer"
+            width={1920}
+            height={1080}
+          />
+      }
+        
+      </motion.div>
+    </section>
+  );
 }
